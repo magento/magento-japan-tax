e85974c9de295d9868ada35f8cc04128eb347687
Nozomi Okada <nozomio@adobe.com>
2023-07-12 19:05:27 +0900

    Skip Subtotal and Shipping collector

---
+++ Plugin/JapanInvoiceShipping.php
@@ -0,0 +1,42 @@
+<?php
+namespace Japan\Tax\Plugin;
+
+use Japan\Tax\Model\CurrencyRoundingFactory;
+use \Magento\Tax\Model\Sales\Total\Quote\Shipping;
+use \Magento\Quote\Model\Quote;
+use \Magento\Quote\Api\Data\ShippingAssignmentInterface;
+use \Magento\Quote\Model\Quote\Address\Total;
+use Psr\Log\LoggerInterface;
+
+class JapanInvoiceShipping
+{
+    /**
+     * @var CurrencyRoundingFactory
+     */
+    private $currencyRoundingFactory;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $_logger;
+
+    public function __construct(
+        CurrencyRoundingFactory $currencyRoundingFactory,
+        LoggerInterface $logger,
+    ) {
+        $this->currencyRoundingFactory = $currencyRoundingFactory;
+        $this->_logger = $logger;
+    }
+
+    public function aroundCollect(
+        Shipping $subject, 
+        callable $proceed,
+        Quote $quote,
+        ShippingAssignmentInterface $shippingAssignment,
+        Total $total,
+    ) {
+        $this->_logger->debug(sprintf('Skipping the default Shipping collector'));
+
+        return $subject;
+    }
+}
---
+++ Plugin/JapanInvoiceSubtotal.php
@@ -0,0 +1,42 @@
+<?php
+namespace Japan\Tax\Plugin;
+
+use Japan\Tax\Model\CurrencyRoundingFactory;
+use \Magento\Tax\Model\Sales\Total\Quote\Subtotal;
+use \Magento\Quote\Model\Quote;
+use \Magento\Quote\Api\Data\ShippingAssignmentInterface;
+use \Magento\Quote\Model\Quote\Address\Total;
+use Psr\Log\LoggerInterface;
+
+class JapanInvoiceSubtotal
+{
+    /**
+     * @var CurrencyRoundingFactory
+     */
+    private $currencyRoundingFactory;
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $_logger;
+
+    public function __construct(
+        CurrencyRoundingFactory $currencyRoundingFactory,
+        LoggerInterface $logger,
+    ) {
+        $this->currencyRoundingFactory = $currencyRoundingFactory;
+        $this->_logger = $logger;
+    }
+
+    public function aroundCollect(
+        Subtotal $subject, 
+        callable $proceed,
+        Quote $quote,
+        ShippingAssignmentInterface $shippingAssignment,
+        Total $total,
+    ) {
+        $this->_logger->debug(sprintf('Skipping the default Subtotal collector'));
+
+        return $subject;
+    }
+}
