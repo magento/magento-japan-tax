3ed5a2ecfe009b4edab071953a771e965ebd5916
Nozomi Okada <nozomio@adobe.com>
2023-08-16 15:13:02 +0900

    ASJDMA-942: Invoice tax in price (#13)
    * Display tax in price when prices include tax
    
    * Display tax in price in invoice PDF

--- Block/Sales/Order/Totals.php
+++ Block/Sales/Order/Totals.php
@@ -1,8 +1,33 @@
 <?php
 namespace Japan\Tax\Block\Sales\Order;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
 class Totals extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        StoreManagerInterface $storeManager,
+        ScopeConfigInterface $scopeConfig,
+        array $data = [],
+    ) {
+        parent::__construct($context, $data);
+        $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManager;
+    }
+
     public function getOrder()
     {
         return $this->getParentBlock()->getOrder();
@@ -10,21 +35,32 @@ class Totals extends \Magento\Framework\View\Element\Template
 
     public function initTotals()
     {
-        // TODO: Switch tax excl. and incl.
+        $taxInclude = (int) $this->scopeConfig->getValue(
+            'tax/calculation/price_includes_tax',
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+
         $taxDataObject = new \Magento\Framework\DataObject(
             [
-                'code' => 'subtotal_excl_jct_10',
-                'value' => $this->getOrder()->getSubtotalExclJct10(),
-                'label' => __('Subtotal Subject to 10% Tax'),
+                'code' => $taxInclude ?
+                    'subtotal_incl_jct_10' : 'subtotal_excl_jct10',
+                'value' => $taxInclude ?
+                    $this->getOrder()->getSubtotalInclJct10() : $this->getOrder()->getSubtotalExclJct10(),
+                'label' => $taxInclude ?
+                    __('Subtotal Subject to 10% Tax (Incl. Tax)') : __('Subtotal Subject to 10% Tax'),
             ]
         );
         $this->getParentBlock()->addTotalBefore($taxDataObject, 'subtotal');
 
         $taxDataObject = new \Magento\Framework\DataObject(
             [
-                'code' => 'subtotal_excl_jct_8',
-                'value' => $this->getOrder()->getSubtotalExclJct8(),
-                'label' => __('Subtotal Subject to 8% Tax'),
+                'code' => $taxInclude ?
+                    'subtotal_incl_jct_8' : 'subtotal_excl_jct_8',
+                'value' => $taxInclude ?
+                    $this->getOrder()->getSubtotalInclJct8() : $this->getOrder()->getSubtotalExclJct8(),
+                'label' => $taxInclude ?
+                    __('Subtotal Subject to 8% Tax (Incl. Tax)') : __('Subtotal Subject to 8% Tax'),
             ]
         );
         $this->getParentBlock()->addTotalBefore($taxDataObject, 'subtotal');
--- Model/Sales/Pdf/Subtotal.php
+++ Model/Sales/Pdf/Subtotal.php
@@ -1,25 +1,65 @@
 <?php
 namespace Japan\Tax\Model\Sales\Pdf;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Tax\Helper\Data;
+use Magento\Tax\Model\Calculation;
+use Magento\Tax\Model\ResourceModel\Sales\Order\Tax\CollectionFactory;
+
 class Subtotal extends \Magento\Sales\Model\Order\Pdf\Total\DefaultTotal
 {
+     /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    public function __construct(
+        Data $taxHelper,
+        Calculation $taxCalculation,
+        CollectionFactory $ordersFactory,
+        StoreManagerInterface $storeManager,
+        ScopeConfigInterface $scopeConfig,
+        array $data = []
+    ) {
+        parent::__construct($taxHelper, $taxCalculation, $ordersFactory, $data);
+        $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManager;
+    }
+
     public function getTotalsForDisplay()
     {
         $fontSize = $this->getFontSize() ? $this->getFontSize() : 7;
-        
-        // TODO: Switch tax excl. and incl.
-        $subtotalJct10 = $this->getOrder()->getSubtotalExclJct10();
-        $subtotalJct8 = $this->getOrder()->getSubtotalExclJct8();
+
+        $taxInclude = (int) $this->scopeConfig->getValue(
+            'tax/calculation/price_includes_tax',
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
 
         $subtotalInfo = [
             [
-                'amount' => $this->getAmountPrefix() . $this->getOrder()->formatPriceTxt($subtotalJct10),
-                'label' => __('Subtotal Subject to 10% Tax'),
+                'amount' => $this->getAmountPrefix() . $this->getOrder()->formatPriceTxt(
+                    $taxInclude ? 
+                        $this->getOrder()->getSubtotalInclJct10() : $this->getOrder()->getSubtotalExclJct10()
+                ),
+                'label' => $taxInclude ?
+                    __('Subtotal Subject to 10% Tax (Incl. Tax)') : __('Subtotal Subject to 10% Tax'),
                 'font_size' => $fontSize,
             ],
             [
-                'amount' => $this->getAmountPrefix() . $this->getOrder()->formatPriceTxt($subtotalJct8),
-                'label' => __('Subtotal Subject to 8% Tax'),
+                'amount' => $this->getAmountPrefix() . $this->getOrder()->formatPriceTxt(
+                    $taxInclude ?
+                        $this->getOrder()->getSubtotalInclJct8() : $this->getOrder()->getSubtotalExclJct8()
+                ),
+                'label' => $taxInclude ?
+                    __('Subtotal Subject to 8% Tax (Incl. Tax)') : __('Subtotal Subject to 8% Tax'),
                 'font_size' => $fontSize,
             ]
         ];
